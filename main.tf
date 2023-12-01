d516e8b04e0f07767f5fd13411a567a25e64aea9
armzig <bellotaophyc@outlook.com>
2023-12-01 17:30:27 +0100

    updating the local file resource

--- main.tf
+++ main.tf
@@ -15,5 +15,9 @@ resource "local_file" "bookseven" {
         The moral is, as Ts ao Kung and others point out: Do not march a hundred LI to gain a tactical advantage, either with or without impedimenta. 
         
         Maneuvers of this description should be confined to short distances. Stonewall Jackson said: The hardships of forced marches are often more painful than the dangers of battle. He did not often call upon his troops for extraordinary exertions. It was only when he intended a surprise, or when a rapid retreat was imperative, that he sacrificed everything for speed.
+
+        This sentence contains one of those highly condensed and somewhat enigmatical expressions of which Sun Tzu is so fond. This is how it is explained by Ts ao Kung: Make it appear that you are a long way off, then cover the distance rapidly and arrive on the scene before your opponent. 
+        
+        Tu Mu says: Hoodwink the enemy, so that he may be remiss and leisurely while you are dashing along with utmost speed. Ho Shih gives a slightly different turn: Although you may have difficult ground to traverse and natural obstacles to encounter this is a drawback which can be turned into actual advantage by celerity of movement. Signal examples of this saying are afforded by the two famous passages across the Alps—that of Hannibal, which laid Italy at his mercy, and that of Napoleon two thousand years later, which resulted in the great victory of Marengo.
     EOT
 }
